1842c2b4120c8cfe5459e2718da0f7c1a862d39f
lizqjue <lizqjue@gmail.com>
2020-01-14 00:47:36 +0100

    [Added] second challenge

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -5,8 +5,14 @@ fun main(args: Array<String>) {
     do {
         response = init()
         when(response) {
-            "1" -> println("Has seleccionado hacer una receta")
-            "2" -> println("Has seleccionado ver tus recetas")
+            "1" -> {
+                println("Has seleccionado hacer una receta")
+                makeRecipe()
+            }
+            "2" -> {
+                println("Has seleccionado ver tus recetas")
+                viewRecipe({readLine() ?: "0"}())
+            }
             "3" -> println("Saliendo del programa...")
             else -> println("La opción seleccionada no está entre las posibles, inténtalo de nuevo")
         }
@@ -28,4 +34,33 @@ private fun init(): String? {
     var response: String?
     response = readLine() ?: "0"
     return response
+}
+
+private fun makeRecipe() {
+    {println("""
+        Hacer receta
+        Selecciona por categoría el ingrediente que buscas
+        1. Agua
+        2. Leche
+        3. Carne
+        4. Verduras
+        5. Frutas
+        6. Cereal
+        7. Huevos
+        8. Aceites
+    """)}()
+}
+
+private fun viewRecipe(category: String) {
+    println({ category: String -> when(category) {
+        "1" -> "Agua"
+        "2" -> "Leche"
+        "3" -> "Carne"
+        "4" -> "Verduras"
+        "5" -> "Frutas"
+        "6" -> "Cereal"
+        "7" -> "Huevos"
+        "8" -> "Aceites"
+        else -> "La categoría no existe"}
+        }(category))
 }
